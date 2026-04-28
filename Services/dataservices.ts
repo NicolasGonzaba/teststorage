7fcd911d2ed7a414eee9ae0a4bc3ebb1600d0fed
NicolasGonzaba <nicolasgonzaba1@gmail.com>
2026-04-28 10:39:20 -0700

    fixed

--- Services/dataservices.ts
+++ Services/dataservices.ts
@@ -1,22 +1,33 @@
 
-const url = 'http://localhost:5057';
-// const url = 'https://munchrapiv1-fdh6cpdzffe6b5fg.westus3-01.azurewebsites.net/';
+// const url = 'http://localhost:5057';
+// const url = 'http://localhost:5174';
+const url = 'https://munchrapiv1-fdh6cpdzffe6b5fg.westus3-01.azurewebsites.net/';
 const blobURL = "https://munchrblobstorage.blob.core.windows.net/pfpstorage"
 
-export const blobUpload = async (params: FormData)=> {
-        const response = await fetch(url + '/Blob/Upload', {
-            method: 'POST',
-            body: params, 
-        });
+export const blobUpload = async (params: FormData) => {
+    console.log(params.get("file"));
+    console.log(params.get("fileName"));
+    const response = await fetch(url + '/Blob/Upload', {
+        method: 'POST',
+        body: params,
+    });
 
-        if (response.ok) {
-            const fileName = params.get('fileName') as string;
-            
-            const uploadedFileUrl = `${blobURL}/${fileName}`;
-            
-            return uploadedFileUrl;
-        } else {
-            console.log('Failed to upload file.');
-            return null;
-        }
+
+    if (response.ok) {
+        const text = await response.text();
+        console.log("Response:", text);
+        const fileName = params.get('fileName') as string;
+
+        const uploadedFileUrl = `${blobURL}/${fileName}`;
+
+        return uploadedFileUrl;
+        // if (response.ok) {
+        //     const data = await response.json();
+
+        //     return data.fileUrl; // ← real Azure URL
+
+    } else {
+        console.log('Failed to upload file.');
+        return null;
+    }
 };
